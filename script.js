067ee5d958bdfc2824d957ca527b4a98d47f1588
Kalash-Harchandani <kalashharchandani@gmail.com>
2025-02-25 09:53:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const modeSwitch = document.getElementById('mode-switch');
     modeSwitch.addEventListener('click', () => {
-      document.body.classList.toggle('dark-mode');
-      modeSwitch.textContent =
-        document.body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
+        document.body.classList.toggle('dark-mode');
+        modeSwitch.textContent =
+            document.body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
     });
-  });
+});
 
 
 document.addEventListener("DOMContentLoaded", () => {
